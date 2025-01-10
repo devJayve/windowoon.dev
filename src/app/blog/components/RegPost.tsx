7270948d49d5f95d12706e790de0b91a4e7808fc
DevJayve <wndud4576@naver.com>
2025-01-10 15:37:44 +0900

    chore: husky 일부 수정

--- src/app/blog/components/RegPost.tsx
+++ src/app/blog/components/RegPost.tsx
@@ -3,16 +3,11 @@ import Link from 'next/link';
 import { RegPostProps } from '@/types/post';
 
 export default function RegPost({ frontMatter, category, slug }: RegPostProps) {
-  const formattedDate = frontMatter.date.toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
   return (
     <Link href={`/blog/${category}/${slug}`}>
       <div className="flex p-4">
         <div className="grow basis-3/4">
-          <p className="mt-3 text-sm font-light">{formattedDate}</p>
+          <p className="mt-3 text-sm font-light">{frontMatter.date}</p>
           <div className="font-semibold">{frontMatter.title}</div>
         </div>
         <div className="grow basis-1/4"></div>
