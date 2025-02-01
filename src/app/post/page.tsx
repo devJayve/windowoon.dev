00bde9b566ba161829f795cd69ae5b0d2e59cf99
DevJayve <wndud4576@naver.com>
2025-02-01 13:16:06 +0900

    feat: 게시물 아이템 UI 내 카테고리 추가

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,16 +1,18 @@
-import RegPostList from '@/features/post/components/RegPostList';
 import FloatingActionButton from '@/shared/components/button/FloatingActionButton';
 import { getAllPosts } from '@/features/post/lib';
+import PostItem from '@/features/post/components/PostItem';
 
 export default async function BlogPage() {
   const allPosts = await getAllPosts();
 
   return (
     <>
-      <div className="flex flex-col justify-center">
-        <RegPostList posts={allPosts} />
-        <FloatingActionButton />
+      <div className="mx-auto max-w-3xl">
+        {allPosts.map(post => (
+          <PostItem key={post.id} post={post} />
+        ))}
       </div>
+      <FloatingActionButton />
     </>
   );
 }
