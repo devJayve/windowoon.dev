512e84f872539fe2f2e223dd6f609866158652f8
DevJayve <wndud4576@naver.com>
2025-02-03 15:06:21 +0900

    feat: authOptions 별도 config 파일로 분리

---
+++ src/features/auth/config/index.ts
@@ -0,0 +1,42 @@
+import { AuthOptions } from 'next-auth';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { db } from '@/db/drizzle';
+import { accounts, authenticators, sessions, users, verificationTokens } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions: AuthOptions = {
+  adapter: DrizzleAdapter(db.$primary, {
+    usersTable: users,
+    accountsTable: accounts,
+    authenticatorsTable: authenticators,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  callbacks: {
+    async session({ session }) {
+      if (!session.user.email) return session;
+
+      const [selectedUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, session.user.email))
+        .limit(1);
+
+      session.user = selectedUser;
+      return session;
+    },
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: 'database',
+  },
+  pages: {
+    signIn: '/login',
+  },
+};
