2bc9cfa2112d9f25e8fdebb02db4282a8f20c65f
DevJayve <wndud4576@naver.com>
2025-02-11 15:36:31 +0900

    feat: 댓글 DB 스키마 구현

---
+++ src/db/schema/comment.ts
@@ -0,0 +1,68 @@
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  PgColumn,
+  primaryKey,
+} from 'drizzle-orm/pg-core';
+import { PostTable } from './post';
+import { users } from './user';
+
+export const CommentStatus = pgEnum('comment_status', ['published', 'deleted']);
+
+export const ReactionType = pgEnum('reaction_type', [
+  'thumbsup', // 👍
+  'heart', // ❤️
+  'laugh', // 😄
+  'surprise', // 😮
+  'sad', // 😢
+  'angry', // 😠
+]);
+
+export const CommentTable = pgTable('comments', {
+  id: serial('id').primaryKey(),
+  postId: integer('post_id')
+    .notNull()
+    .references(() => PostTable.id, { onDelete: 'cascade' }),
+  userId: text('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  content: text('content').notNull(),
+  status: CommentStatus('status').notNull().default('published'),
+  parentId: integer('parent_id').references((): PgColumn => CommentTable.id, {
+    onDelete: 'cascade',
+  }),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+// 댓글 좋아요 관리를 위한 테이블
+export const CommentReactionTable = pgTable(
+  'comment_reactions',
+  {
+    id: serial('id').primaryKey(),
+    userId: text('user_id')
+      .notNull()
+      .references((): PgColumn => users.id, {
+        onDelete: 'cascade',
+      }),
+    commentId: integer('comment_id')
+      .notNull()
+      .references(() => CommentTable.id, {
+        onDelete: 'cascade',
+      }),
+    type: ReactionType('type').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  },
+  table => {
+    return [
+      {
+        pk: primaryKey({ columns: [table.userId, table.commentId, table.type] }),
+      },
+    ];
+  },
+);
