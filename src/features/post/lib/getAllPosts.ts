e842d52c3950a6b4862cf3c94453e18cb15013df
DevJayve <wndud4576@naver.com>
2025-03-06 16:38:50 +0900

    fix: admonition 내에 볼드체 CSS 수정

--- src/features/post/lib/getAllPosts.ts
+++ src/features/post/lib/getAllPosts.ts
@@ -1,31 +1,27 @@
-import { PostMeta } from '@/features/post/types';
 import { db } from '@/db/drizzle';
 import { PostTable } from '@/db/schema';
 import { desc, eq, or } from 'drizzle-orm';
-import { unstable_cache } from 'next/cache';
 
 export async function getAllPosts() {
-  return unstable_cache(async (): Promise<PostMeta[]> => {
-    const isDevelopment = process.env.NODE_ENV === 'development';
+  const isDevelopment = process.env.NODE_ENV === 'development';
 
-    const viewModeCondition = isDevelopment
-      ? or(eq(PostTable.viewMode, 'public'), eq(PostTable.viewMode, 'development'))
-      : eq(PostTable.viewMode, 'public');
+  const viewModeCondition = isDevelopment
+    ? or(eq(PostTable.viewMode, 'public'), eq(PostTable.viewMode, 'development'))
+    : eq(PostTable.viewMode, 'public');
 
-    return db
-      .select({
-        id: PostTable.id,
-        title: PostTable.title,
-        description: PostTable.description,
-        slug: PostTable.slug,
-        views: PostTable.views,
-        viewMode: PostTable.viewMode,
-        createdAt: PostTable.createdAt,
-        updatedAt: PostTable.updatedAt,
-        categories: PostTable.categories,
-      })
-      .from(PostTable)
-      .where(viewModeCondition)
-      .orderBy(desc(PostTable.createdAt));
-  })();
+  return db
+    .select({
+      id: PostTable.id,
+      title: PostTable.title,
+      description: PostTable.description,
+      slug: PostTable.slug,
+      views: PostTable.views,
+      viewMode: PostTable.viewMode,
+      createdAt: PostTable.createdAt,
+      updatedAt: PostTable.updatedAt,
+      categories: PostTable.categories,
+    })
+    .from(PostTable)
+    .where(viewModeCondition)
+    .orderBy(desc(PostTable.createdAt));
 }
